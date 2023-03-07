2b721f51c392de9734e753a27bda9f39b2dc29e5
PaolaBramlett <paolabramlett@gmail.com>
2023-03-06 21:32:55 -0600

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import img from './img.jpg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
+        <h1>
+          Hola, soy Paola
+        </h1>
+        <img src={img} className="img-app" alt="app-img" />
+     
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bienvenid@ a mi app de React
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
       </header>
     </div>
   );
